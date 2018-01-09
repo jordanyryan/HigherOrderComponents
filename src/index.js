2631ce6309e7b878035aad91fdab5d441683f3d9
Jordany Rosas <jordanyryan@gmail.com>
2018-01-09 14:59:06 -0500

    Set up headers

--- src/index.js
+++ src/index.js
@@ -3,16 +3,27 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './reducers';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import promise from 'redux-promise';
+
+import Resources from './components/resources';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
+  
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+        <div>
+          <Switch>
+            <Route path="/" component={App}>
+              <Route path="resources" component={Resources} />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
   </Provider>
   ,
   document.getElementById('root')
